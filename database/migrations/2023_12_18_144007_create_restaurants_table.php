39fe49c584ff8b2521e339b2daf4765ff922bceb
mohammed-abozamel112 <mohammed-abozamel112@gmail.com>
2023-12-18 22:08:53 +0200

    tabels

--- database/migrations/2023_12_18_144007_create_restaurants_table.php
+++ database/migrations/2023_12_18_144007_create_restaurants_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('type', ['manager','customer'])->default('customer');
-            $table->string('mobile')->unique();
-            $table->rememberToken();
+            $table->string('tax-number');
+            $table->string('owner');
+            $table->string('address');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('restaurants');
     }
 };
